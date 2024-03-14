e1b5988a379792800a6bbdacc7a6a76af9efa186
Gustavo <gustavomacedosousa013@gmail.com>
2024-03-14 18:16:37 -0300

    Finalizando slider grande

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-document.getElementById('form_header').addEventListener('submit', function(e) {
+document.getElementById('form_header').addEventListener('submit', function (e) {
     e.preventDefault();
     // Coloque aqui o código que você quer executar quando o formulário for submetido
-  });
+});
 
 var botao1 = document.getElementById('botao_formulario');
 var input_form = document.getElementById('input_formulario');
 var div_form = document.getElementById('form_header');
 
-botao1.addEventListener('click', function() {
-        if (input_form.style.width == '0px'){
+botao1.addEventListener('click', function () {
+    if (input_form.style.width == '0px') {
         input_form.style.width = '200px';
         input_form.style.transform = 'all 0.5s ease-in-out';
         botao1.style.height = '30px';
@@ -28,7 +28,6 @@ botao1.addEventListener('click', function() {
         botao1.style.paddingRight = '0px';
     }
 });
-
 function funcao_Sliders(slider_interno_Id, botao1_slider_Id, botao1_slider_img_Id, botao2_slider_Id, botao2_slider_img_Id) {
     var totalMargin = 0;
     var sliderDiv = document.getElementById(slider_interno_Id);
@@ -37,16 +36,16 @@ function funcao_Sliders(slider_interno_Id, botao1_slider_Id, botao1_slider_img_I
     var button2 = document.getElementById(botao2_slider_Id);
     var button2Img = document.getElementById(botao2_slider_img_Id);
 
-    button1.addEventListener('mouseover', function() {
+    button1.addEventListener('mouseover', function () {
         button1Img.style.display = 'block';
     });
 
-    button1.addEventListener('mouseout', function() {
+    button1.addEventListener('mouseout', function () {
         button1Img.style.display = 'none';
         button1Img.style.transform = 'scale(1.0)';
     });
 
-    button1.addEventListener('click', function() {
+    button1.addEventListener('click', function () {
         if (sliderDiv.style.marginLeft < -16006 + 'px') {
             totalMargin = 0;
             sliderDiv.style.marginLeft = totalMargin + 'px';
@@ -56,18 +55,18 @@ function funcao_Sliders(slider_interno_Id, botao1_slider_Id, botao1_slider_img_I
         }
     });
 
-    button2.addEventListener('mouseover', function() {
+    button2.addEventListener('mouseover', function () {
         button2Img.style.display = 'block';
     });
 
-    button2.addEventListener('mouseout', function() {
+    button2.addEventListener('mouseout', function () {
         button2Img.style.display = 'none';
         button2Img.style.transform = 'scale(1.0)';
     });
 
     sliderDiv.style.marginLeft = '0px';
 
-    button2.addEventListener('click', function() {
+    button2.addEventListener('click', function () {
         if (sliderDiv.style.marginLeft == 0 + 'px') {
             totalMargin = -12306;
             sliderDiv.style.marginLeft = totalMargin + 'px';
@@ -77,7 +76,55 @@ function funcao_Sliders(slider_interno_Id, botao1_slider_Id, botao1_slider_img_I
         }
     });
 }
+function funcao_Sliders_Secao4(slider_interno_Id, botao1_slider_Id, botao1_slider_img_Id, botao2_slider_Id, botao2_slider_img_Id) {
+    var totalMargin = 0;
+    var sliderDiv = document.getElementById(slider_interno_Id);
+    var button1 = document.getElementById(botao1_slider_Id);
+    var button1Img = document.getElementById(botao1_slider_img_Id);
+    var button2 = document.getElementById(botao2_slider_Id);
+    var button2Img = document.getElementById(botao2_slider_img_Id);
 
+    button1.addEventListener('mouseover', function () {
+        button1Img.style.display = 'block';
+    });
+
+    button1.addEventListener('mouseout', function () {
+        button1Img.style.display = 'none';
+        button1Img.style.transform = 'scale(1.0)';
+    });
+
+    button1.addEventListener('click', function () {
+        console.log(totalMargin)
+        if (sliderDiv.style.marginLeft < '-18058px') {
+
+        } else {
+            console.log(totalMargin)
+            totalMargin = totalMargin - 1700;
+            console.log(totalMargin)
+            sliderDiv.style.marginLeft = totalMargin + 'px';
+        }
+    });
+
+    button2.addEventListener('mouseover', function () {
+        button2Img.style.display = 'block';
+    });
+
+    button2.addEventListener('mouseout', function () {
+        button2Img.style.display = 'none';
+        button2Img.style.transform = 'scale(1.0)';
+    });
+
+    var totalMargin = -1;
+    button2.addEventListener('click', function () {
+        sliderDiv.style.marginLeft = totalMargin + 'px';
+
+        if (totalMargin == -22915) {
+            totalMargin = -1;
+        } else {
+            totalMargin += -1206;
+        }
+    });
+}
 /*SLIDER SECAO 1 */
 funcao_Sliders('div_slider_interno', 'botao1_slider', 'botao1_slider_img', 'botao2_slider', 'botao2_slider_img');
 
@@ -87,9 +134,13 @@ funcao_Sliders('div_slider_secao2_interno', 'botao1_slider_secao2', 'botao1_slid
 /*SLIDER SECAO 3 */
 funcao_Sliders('div_slider_secao3_interno', 'botao1_slider_secao3', 'botao1_slider_secao3_img', 'botao2_slider_secao3', 'botao2_slider_secao3_img');
 
+/* SLIDER SECAO 4 */
+funcao_Sliders_Secao4('div_slider_secao4_interno', 'botao1_slider_secao4', 'botao1_slider_secao4_img', 'botao2_slider_secao4', 'botao2_slider_secao4_img');
 
 
 
+/*SLIDER SECAO 5 */
+funcao_Sliders('div_slider_secao5_interno', 'botao1_slider_secao5', 'botao1_slider_secao5_img', 'botao2_slider_secao5', 'botao2_slider_secao5_img');
 
 /*SLIDER SECAO 6 */
 funcao_Sliders('div_slider_secao6_interno', 'botao1_slider_secao6', 'botao1_slider_secao6_img', 'botao2_slider_secao6', 'botao2_slider_secao6_img');
